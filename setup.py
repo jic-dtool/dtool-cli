6c5e0117e5d1224ef6cf7cb3cdd46b22a6bef87b
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-07-28 13:49:10 +0100

    Re-format setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ url = ""
 version = "0.1.0"
 readme = open('README.rst').read()
 
-setup(name="dtool-cli",
-      packages=["dtool_cli"],
-      version=version,
-      description="Data management command line interface: dtool.",
-      long_description=readme,
-      include_package_data=True,
-      author="Tjelvar Olsson",
-      author_email="tjelvar.olsson@jic.ac.uk",
-      url=url,
-      install_requires=[],
-      download_url="{}/tarball/{}".format(url, version),
-      license="MIT")
+setup(
+    name="dtool-cli",
+    packages=["dtool_cli"],
+    version=version,
+    description="Data management command line interface: dtool.",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
