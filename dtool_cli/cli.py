0424f603ee7c034404dcb710cf963297d1da286a
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-02 10:07:11 +0100

    Add dataset_path_argument click decorator

--- dtool_cli/cli.py
+++ dtool_cli/cli.py
@@ -1,9 +1,31 @@
 """Skeleton defining the dtool plugin entry points."""
 
+import os
 from pkg_resources import iter_entry_points
 
 import click
 from click_plugins import with_plugins
+
+import dtoolcore
+
+
+def _path_is_dataset(ctx, param, value):
+    abspath = os.path.abspath(value)
+    if not os.path.isdir(abspath):
+        raise click.BadParameter(
+            "Not a directory: {}".format(abspath))
+    try:
+        dtoolcore.DataSet.from_path(abspath)
+    except (dtoolcore.DtoolTypeError, dtoolcore.NotDtoolObject) as e:
+        raise click.BadParameter(
+            "Not a dataset: {}".format(e))
+    return abspath
+
+
+
+dataset_path_argument = click.argument(
+    'dataset_path',
+    callback=_path_is_dataset)
 
 
 @click.group()
