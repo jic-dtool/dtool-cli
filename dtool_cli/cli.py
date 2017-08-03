e676a0b8bdfe924d80438fb28501087d9d4ec9ac
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-03 13:12:37 +0100

    Add pretty version text

--- dtool_cli/cli.py
+++ dtool_cli/cli.py
@@ -8,6 +8,8 @@ from click_plugins import with_plugins
 
 import dtoolcore
 
+from . import __version__
+
 
 def _path_is_dataset(ctx, param, value):
     abspath = os.path.abspath(value)
@@ -22,13 +24,27 @@ def _path_is_dataset(ctx, param, value):
     return abspath
 
 
-
 dataset_path_argument = click.argument(
     'dataset_path',
     callback=_path_is_dataset)
 
 
+def pretty_version_text():
+    """Return pretty version text listing all plugins."""
+    modules = [ep.module_name for ep in iter_entry_points("dtool.dataset")]
+    modules = modules + [ep.module_name for ep in iter_entry_points("dtool.collection")]
+    packages = set([m.split(".")[0] for m in modules])
+    version_lines = ["dtool, version {}".format(__version__)]
+    version_lines.append("\nCore:")
+    version_lines.append("dtoolcore, version {}".format(dtoolcore.__version__))
+    version_lines.append("\nPlugins:")
+    for p in packages:
+        dyn_load_p = __import__(p)
+        version_lines.append("{}, version {}".format(p,  dyn_load_p.__version__))
+    return "\n".join(version_lines)
+
 @click.group()
+@click.version_option(message=pretty_version_text())
 def dtool():
     """Manage data as datasets and collections."""
 
